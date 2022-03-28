14bab2b07c47eb87bd66e4dec261dd7c45955c13
unknown <dhruvagarwal@usquare.info>
2022-03-28 22:17:16 +0530

    1st code added

---
+++ 1.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+contract test4 {
+
+    mapping(address => uint) public balanceReceived;
+    address payable owner = payable(msg.sender);
+    address payable myAddress;
+    address payable feeAddress;  
+    
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function setMyAddress(address payable _myAddress) public payable {
+        require(msg.sender == owner, "you are not the owner");
+        myAddress = _myAddress;
+    }
+    function setFeeAddress(address payable _feeAddress) public payable {
+        require(msg.sender == owner, "you are not the owner");
+        feeAddress = _feeAddress;
+    }
+    function receiveMoney() public payable {
+        require(msg.sender != myAddress, "you cannot call function");
+        myAddress.transfer(msg.value / 2);
+        feeAddress.transfer((msg.value / 2) * 10 / 100);
+        balanceReceived[msg.sender] = msg.value;
+        
+    }
+    function getBalance () public view returns (uint) {
+        return address(this).balance;
+    }
+    function withdrawAllMoney() public {
+        require(msg.sender == owner, "not owner");
+        payable(msg.sender).transfer(getBalance());
+    }
+}
